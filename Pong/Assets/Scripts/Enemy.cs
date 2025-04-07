82d8fc316bf9985eafebaf0dc13662051ad2af6e
Drew <andrew.bristow.2004@gmail.com>
2025-04-07 16:28:18 +0100

    AI Project
    Project initialisation.

---
+++ Pong/Assets/Scripts/Enemy.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : Agent
+{
+    // Start is called before the first frame update
+    public override void Initialize()
+    {
+
+    }
+
+    public override void CollectObservations(VectorSensor sensor)
+    {
+
+    }
+
+    public override void OnActionReceived(ActionBuffers actionBuffers)
+    {
+
+    }
+
+    public override void OnEpisodeBegin()
+    {
+
+    }
+
+    }
